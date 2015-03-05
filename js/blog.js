c53bcef793095785969c70d5d213e998ca7c9935
Glenn Manghi <gmanghi@gmail.com>
2015-03-05 18:25:45 +0800

    Update

--- js/blog.js
+++ js/blog.js
@@ -3,7 +3,7 @@ $(function() {
     Parse.$ = jQuery;
  
     // Replace this line with the one on your Quickstart Guide Page
-    Parse.initialize("buWMcHVkpXrUcQ8LlyY8t4nTQxaHLY10w97A09bH", "2hhbkfu8YDfOexCcY7g79rEg0QCvv0uNm1b6dl2q");
+    Parse.initialize("cOSipw6bHqAKAXfhCaerP9rvNuw3cRg0U8JdgzrR", "kmW4P3eAlvfOWs8zGxrjh1XjTUVTctBXAENW8ide");
  
     var TestObject = Parse.Object.extend("TestObject");
     var testObject = new TestObject();
